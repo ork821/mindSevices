f4dad4c96ec72593bf79909443a07439f7d9f242
ork821 <ork821@mail.ru>
2019-07-03 11:19:57 +0300

    Now we can ply with it! On click 'Show Again' or 'New Word', app show word for a few seconds and then hide. Added input field and button to enter reversed word and test it. If it's ok, then shows new word, else shows old word again

--- src/WordsReverse/Words.js
+++ src/WordsReverse/Words.js
@@ -1,62 +1,87 @@
 import React, {Component} from 'react';
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import {Container, Row, Col, Button, FormControl, InputGroup} from 'react-bootstrap';
 import Word from './Word';
 
 
-
-export default class Words extends Component {
+class Words extends Component {
 
 
     constructor(props) {
         super(props);
         this.state = {
-            word: "Привет!",
+            word: "Hello! Click 'New Word' to get started",
             isVisible: true,
+            showAgain: true,
+            newWord: false,
+            reverse: ''
+
         }
     };
 
     nextWord = () => {
         const {words} = this.props;
         this.setState({
-            word:  words[Math.floor(Math.random() * (words.length))],
-            isVisible: true
+            word: words[Math.floor(Math.random() * (words.length))],
+            isVisible: true,
+            newWord: true,
+            showAgain: true
         });
+        const timer = setTimeout(this.Hide, 2000);
     };
-
-
 
 
     showAgain = () => {
         this.setState({
             isVisible: true
         });
+        const timer = setTimeout(this.Hide, 1500);
     };
 
-    Hide = () =>  {
+    Hide = () => {
         this.setState({
-            isVisible: false
-        })
+            isVisible: false,
+            newWord: false,
+            showAgain: false,
+        });
     }
 
-    componentDidMount() {
-        setTimeout(this.Hide, 3000);
+    onChange = (e) => {
+        this.setState({
+            reverse: e.target.value
+        });
     }
 
-    componentDidUpdate(){
-        setTimeout(this.Hide, 3000);
+    handleClick = () => {
+        let word = this.state.word;
+        let reverse = this.state.reverse;
+        word = word.split("").reverse().join("");
+        if (word === reverse) {
+            this.setState({
+                reverse: '', //input in clear
+                showAgain: true, //disables button
+            });
+            alert('Great! Lets Try New Word');
+            this.nextWord();
+        } else {
+            alert('Wrong, try again!');
+            this.setState({
+                reverse: '', //input in clear
+            });
+            this.showAgain();
+        }
     }
-
 
 
     render() {
-        return(
+        return (
             <Container>
-                <Row  className="justify-content-md-center">
+                <Row className="justify-content-md-center">
                     <h2>Words Reverse</h2>
                 </Row>
-                <Row  className="justify-content-md-center">
+                <Row className="justify-content-md-center">
                     <Col md="auto">
-                        <Button variant="danger" onClick={this.showAgain}>Show Again</Button>
+                        <Button variant="danger" disabled={this.state.showAgain} onClick={this.showAgain}>Show
+                            Again</Button>
                     </Col>
 
                     <Col lg="5">
@@ -64,7 +89,24 @@ export default class Words extends Component {
                     </Col>
 
                     <Col md="auto">
-                        <Button variant="success" onClick={this.nextWord}>Next Word</Button>
+                        <Button variant="success" disabled={this.state.newWord} onClick={this.nextWord}>Next
+                            Word</Button>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center">
+                    <Col lg="8">
+                        <InputGroup>
+                            <FormControl
+                                placeholder="If word is 'site', then type 'etis'"
+                                aria-label="Reversed Word"
+                                aria-describedby="basic-addon2"
+                                onChange={this.onChange}
+                                value={this.state.reverse}
+                            />
+                            <InputGroup.Append>
+                                <Button variant="outline-success" onClick={this.handleClick}>Test</Button>
+                            </InputGroup.Append>
+                        </InputGroup>
                     </Col>
                 </Row>
             </Container>
@@ -73,3 +115,5 @@ export default class Words extends Component {
         )
     }
 }
+
+export default Words;
