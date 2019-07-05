61ef2235551fc40186dee60fc0f549040415e1e9
ork821 <ork821@mail.ru>
2019-07-05 17:00:55 +0300

    Creating array on change select to easy/medium/hard Added button Hide/Show for buttons with numbers

--- src/Memory/Field.js
+++ src/Memory/Field.js
@@ -1,32 +1,53 @@
 import React, {Component} from 'react';
-import {Button, Col, Row} from 'react-bootstrap';
+import {Button, Col, Row, Alert} from 'react-bootstrap';
 
-export default class Field extends Component{
+export default class Field extends Component {
     //Component gets number (3,4 or 5) and create field 3by3, 4by4 or 5by5. It depends on selected difficulty
     constructor(props) {
         super(props);
         this.state = {
             variant: 'primary',
+            begin: 0
         };
     }
+
+    handleClick = (number) => {
+        console.log(number);
+    }
+
 
 
     render() {
         const size = this.props.difficult;
         const list = [];
         const newArr = this.props.array
+        let isVisible = this.props.isVisible;
 
         for (let i = 0; i < size; i++) {
             const listButtons = [];
             for (let j = 0; j < size; j++) {
-                listButtons.push(
-                    <Button block variant={this.props.variant} size="lg"block>{newArr[i][j]}
-                    </Button>)
+                if (isVisible) {
+                    listButtons.push(
+                        <Alert
+                               variant={this.state.variant}>
+                            {newArr[i][j]}
+                        </Alert>)
+                } else {
+                    listButtons.push(
+                        <Alert
+                               variant={this.state.variant}
+                               onClick={() => this.handleClick(newArr[i][j])}>
+                            ?
+                        </Alert>)
                 }
-            list.push(<Col md="auto">{listButtons.map(el => {return el})}</Col>);
+            }
+            list.push(<Col md="auto">{listButtons.map(el => {
+                return el
+            })}</Col>);
         }
 
-        return(
+
+        return (
             <Row className="justify-content-md-center">
                 {list.map(el => {
                     return el;
--- src/Memory/Memory.js
+++ src/Memory/Memory.js
@@ -1,20 +1,40 @@
 import React, {Component} from 'react';
-import {Container, Row, Col, ProgressBar, Form} from 'react-bootstrap';
+import {Container, Row, Col, Button, Form} from 'react-bootstrap';
 import Field from './Field'
 
 export default class Memory extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            difficult: 3,
+            difficult: 0,
             array: [],
-            newArr: []
+            newArr: [],
+            btnState: true //if true => 'Hide' button
         };
     }
 
     onChange = (e) => {
+        const size = e.target.value;
+        const arr = [];
+        for (let i = 1; i < ((size ** 2) + 1); i++) {
+            arr.push(i);
+        }
+
+        let newArr = [];
+        for (let i = 0; i < size; i++) {
+            const tmp_arr = []
+            for (let j = 0; j < size; j++) {
+                let pos = Math.floor(Math.random() * (arr.length));
+                tmp_arr.push(arr[pos]);
+                arr.splice(pos, 1);
+            }
+            newArr.push(tmp_arr);
+        }
+
         this.setState({
-            difficult: e.target.value,
+            difficult: size,
+            newArr: newArr,
+            btnState: true
         });
 
     };
@@ -36,40 +56,29 @@ export default class Memory extends Component {
                 arr.splice(pos, 1);
             }
             newArr.push(tmp_arr);
-            console.log('newArr', newArr)
         }
+        this.setState({
+            newArr: newArr
+        })
 
+    }
+
+    handleClick = () => {
+        this.setState({
+            btnState: !this.state.btnState
+        })
     }
 
 
 
 
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevState.difficult !== this.state.difficult){
-            this.arrayOperation();
-        }
-    }
 
 
     render() {
-        const arr = [];
-        const size = this.state.difficult;
-        for (let i = 1; i < ((size ** 2) + 1); i++) {
-            arr.push(i);
-        }
 
-        let newArr = [];
-        for (let i = 0; i < size; i++) {
-            const tmp_arr = []
-            for (let j = 0; j < size; j++) {
-                let pos = Math.floor(Math.random() * (arr.length));
-                tmp_arr.push(arr[pos]);
-                arr.splice(pos, 1);
-            }
-            newArr.push(tmp_arr);
-            console.log('newArr', newArr)
-        }
+        let btnState = this.state.btnState
+        let btnText = btnState ? 'Hide' : 'Show'
+        let variant = btnState ? 'secondary' : 'success'
 
         return (
             <Container>
@@ -77,19 +86,26 @@ export default class Memory extends Component {
                     <h2>Improve your memory</h2>
                 </Row>
                 <Row className="justify-content-md-center">
-                    <Col sm="9">
+                    <Col sm="8">
                         <Form.Group controlId="exampleForm.ControlSelect1">
                             <Form.Label>Difficulty</Form.Label>
                             <Form.Control as="select" onChange={this.onChange}>
+                                <option value='0'>Choose difficulty</option>
                                 <option value='3'>Easy</option>
                                 <option value='4'>Medium</option>
                                 <option value='5'>Hard</option>
                             </Form.Control>
                         </Form.Group>
+                        <Button variant={variant}
+                                onClick={this.handleClick}>{btnText}
+                        </Button>
                     </Col>
+
+
+
                 </Row>
 
-                <Field difficult={this.state.difficult} array={newArr}/>
+                <Field difficult={this.state.difficult} array={this.state.newArr} isVisible={this.state.btnState}/>
 
             </Container>
         )
